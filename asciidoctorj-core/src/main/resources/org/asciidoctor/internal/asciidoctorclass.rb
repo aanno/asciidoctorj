db7816e0f049170d8c578c88a3000642b88069cf
Thomas Pasch <thomas.pasch@gmx.de>
2018-12-26 10:43:10 +0100

    fix tests

--- asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
+++ asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
@@ -1,5 +1,9 @@
 module AsciidoctorJ
     include_package 'org.asciidoctor'
+    # the 'api' sub-package is new (formerly not present)
+    include_package 'org.asciidoctor.api'
+
+    # new 'Api' namespace
     module Api
         include_package 'org.asciidoctor.api'
         module Extensions
@@ -9,8 +13,15 @@ module AsciidoctorJ
             TreeProcessor = Treeprocessor unless defined? TreeProcessor
         end
     end
+
+    # old 'Extensions' namespace: should include ..extension and ..api.extension
     module Extensions
         include_package 'org.asciidoctor.extension'
+        # the 'api.extension' sub-package is new (formerly not present)
+        include_package 'org.asciidoctor.api.extension'
+        # Treeprocessor was renamed in to TreeProcessor in https://github.com/asciidoctor/asciidoctor/commit/f1dd816ade9db457b899581841e4cf7b788aa26d
+        # This is necessary to run against both Asciidoctor 1.5.5 and 1.5.6
+        TreeProcessor = Treeprocessor unless defined? TreeProcessor
     end
 end
 
