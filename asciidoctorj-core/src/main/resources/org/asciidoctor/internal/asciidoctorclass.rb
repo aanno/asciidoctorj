e5b6fae1d311787bbf848db64c721f733ba928b6
Thomas Pasch <thomas.pasch@gmx.de>
2018-12-26 14:27:44 +0100

    simplify asciidocclass.rb

--- asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
+++ asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
@@ -1,10 +1,3 @@
-java_import java.math.BigDecimal
-java_import java.math.BigInteger
-
-defined?(::BigDecimal) or require 'bigdecimal'
-
-# include_package 'java.math'
-
 module AsciidoctorJ
     java_import(
         org.asciidoctor.api.Attributes,
@@ -20,26 +13,6 @@ module AsciidoctorJ
     # include_package 'org.asciidoctor'
     # the 'api' sub-package is new (formerly not present)
     # include_package 'org.asciidoctor.api'
-
-=begin
-    Attributes = org.asciidoctor.api.Attributes
-    AttributesBuilder = org.asciidoctor.api.AttributesBuilder
-    CompatMode = org.asciidoctor.api.CompatMode
-    DirectoryWalker = org.asciidoctor.api.DirectoryWalker
-    Options = org.asciidoctor.api.Options
-    OptionsBuilder = org.asciidoctor.api.OptionsBuilder
-    Placement = org.asciidoctor.api.Placement
-    SafeMode = org.asciidoctor.api.SafeMode
-
-    org.asciidoctor.Attributes = org.asciidoctor.api.Attributes
-    org.asciidoctor.AttributesBuilder = org.asciidoctor.api.AttributesBuilder
-    org.asciidoctor.CompatMode = org.asciidoctor.api.CompatMode
-    org.asciidoctor.DirectoryWalker = org.asciidoctor.api.DirectoryWalker
-    org.asciidoctor.Options = org.asciidoctor.api.Options
-    org.asciidoctor.OptionsBuilder = org.asciidoctor.api.OptionsBuilder
-    org.asciidoctor.Placement = org.asciidoctor.api.Placement
-    org.asciidoctor.SafeMode = org.asciidoctor.api.SafeMode
-=end
 
     # old 'Extensions' namespace: should include ..extension and ..api.extension
     module Extensions
@@ -76,56 +49,6 @@ module AsciidoctorJ
         # Treeprocessor was renamed in to TreeProcessor in https://github.com/asciidoctor/asciidoctor/commit/f1dd816ade9db457b899581841e4cf7b788aa26d
         # This is necessary to run against both Asciidoctor 1.5.5 and 1.5.6
         TreeProcessor = Treeprocessor unless defined? TreeProcess
-
-=begin
-        BaseProcessor = org.asciidoctor.api.extension.BaseProcessor
-        BlockMacroProcessor = org.asciidoctor.api.extension.BlockMacroProcessor
-        BlockProcessor = org.asciidoctor.api.extension.BlockProcessor
-        Contexts = org.asciidoctor.api.extension.Contexts
-        DefaultAttribute = org.asciidoctor.api.extension.DefaultAttribute
-        DefaultAttributes = org.asciidoctor.api.extension.DefaultAttributes
-        DocinfoProcessor = org.asciidoctor.api.extension.DocinfoProcessor
-        ExtensionGroup = org.asciidoctor.api.extension.ExtensionGroup
-        Format = org.asciidoctor.api.extension.Format
-        FormatType = org.asciidoctor.api.extension.FormatType
-        IncludeProcessor = org.asciidoctor.api.extension.IncludeProcessor
-        InlineMacroProcessor = org.asciidoctor.api.extension.InlineMacroProcessor
-        Location = org.asciidoctor.api.extension.Location
-        LocationType = org.asciidoctor.api.extension.LocationType
-        MacroProcessor = org.asciidoctor.api.extension.MacroProcessor
-        Name = org.asciidoctor.api.extension.Name
-        PositionalAttributes = org.asciidoctor.api.extension.PositionalAttributes
-        Postprocessor = org.asciidoctor.api.extension.Postprocessor
-        Preprocessor = org.asciidoctor.api.extension.Preprocessor
-        PreprocessorReader = org.asciidoctor.api.extension.PreprocessorReader
-        Processor = org.asciidoctor.api.extension.Processor
-        Reader = org.asciidoctor.api.extension.Reader
-        Treeprocessor = org.asciidoctor.api.extension.Treeprocessor
-
-        org.asciidoctor.extension.BaseProcessor = org.asciidoctor.api.extension.BaseProcessor
-        org.asciidoctor.extension.BlockMacroProcessor = org.asciidoctor.api.extension.BlockMacroProcessor
-        org.asciidoctor.extension.BlockProcessor = org.asciidoctor.api.extension.BlockProcessor
-        org.asciidoctor.extension.Contexts = org.asciidoctor.api.extension.Contexts
-        org.asciidoctor.extension.DefaultAttribute = org.asciidoctor.api.extension.DefaultAttribute
-        org.asciidoctor.extension.DefaultAttributes = org.asciidoctor.api.extension.DefaultAttributes
-        org.asciidoctor.extension.DocinfoProcessor = org.asciidoctor.api.extension.DocinfoProcessor
-        org.asciidoctor.extension.ExtensionGroup = org.asciidoctor.api.extension.ExtensionGroup
-        org.asciidoctor.extension.Format = org.asciidoctor.api.extension.Format
-        org.asciidoctor.extension.FormatType = org.asciidoctor.api.extension.FormatType
-        org.asciidoctor.extension.IncludeProcessor = org.asciidoctor.api.extension.IncludeProcessor
-        org.asciidoctor.extension.InlineMacroProcessor = org.asciidoctor.api.extension.InlineMacroProcessor
-        org.asciidoctor.extension.Location = org.asciidoctor.api.extension.Location
-        org.asciidoctor.extension.LocationType = org.asciidoctor.api.extension.LocationType
-        org.asciidoctor.extension.MacroProcessor = org.asciidoctor.api.extension.MacroProcessor
-        org.asciidoctor.extension.Name = org.asciidoctor.api.extension.Name
-        org.asciidoctor.extension.PositionalAttributes = org.asciidoctor.api.extension.PositionalAttributes
-        org.asciidoctor.extension.Postprocessor = org.asciidoctor.api.extension.Postprocessor
-        org.asciidoctor.extension.Preprocessor = org.asciidoctor.api.extension.Preprocessor
-        org.asciidoctor.extension.PreprocessorReader = org.asciidoctor.api.extension.PreprocessorReader
-        org.asciidoctor.extension.Processor = org.asciidoctor.api.extension.Processor
-        org.asciidoctor.extension.Reader = org.asciidoctor.api.extension.Reader
-        org.asciidoctor.extension.Treeprocessor = org.asciidoctor.api.extension.Treeprocessor
-=end
     end
 
     module Ast
