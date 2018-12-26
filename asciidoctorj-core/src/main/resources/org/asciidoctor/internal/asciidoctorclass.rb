3a6bdc56152060a4626631c333f2f6d4d0e0ea30
Thomas Pasch <thomas.pasch@gmx.de>
2018-12-26 11:10:19 +0100

    trying java_import (unneeded)

--- asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
+++ asciidoctorj-core/src/main/resources/org/asciidoctor/internal/asciidoctorclass.rb
@@ -3,6 +3,57 @@ module AsciidoctorJ
     # the 'api' sub-package is new (formerly not present)
     include_package 'org.asciidoctor.api'
 
+    java_import(
+        org.asciidoctor.api.Attributes,
+        org.asciidoctor.api.AttributesBuilder,
+        org.asciidoctor.api.CompatMode,
+        org.asciidoctor.api.DirectoryWalker,
+        org.asciidoctor.api.Options,
+        org.asciidoctor.api.OptionsBuilder,
+        org.asciidoctor.api.Placement,
+        org.asciidoctor.api.SafeMode
+        )
+
+    module Ast
+      include_package 'org.asciidoctor.ast'
+      include_package 'org.asciidoctor.api.ast'
+
+      java_import(
+          org.asciidoctor.api.ast.Author,
+          org.asciidoctor.api.ast.Block,
+          org.asciidoctor.api.ast.Cell,
+          org.asciidoctor.api.ast.Column,
+          org.asciidoctor.api.ast.ContentModel,
+          org.asciidoctor.api.ast.Cursor,
+          org.asciidoctor.api.ast.DescriptionList,
+          org.asciidoctor.api.ast.DescriptionListEntry,
+          org.asciidoctor.api.ast.Document,
+          org.asciidoctor.api.ast.DocumentHeader,
+          org.asciidoctor.api.ast.List,
+          org.asciidoctor.api.ast.ListItem,
+          org.asciidoctor.api.ast.PhraseNode,
+          org.asciidoctor.api.ast.RevisionInfo,
+          org.asciidoctor.api.ast.Row,
+          org.asciidoctor.api.ast.Section,
+          org.asciidoctor.api.ast.StructuralNode,
+          org.asciidoctor.api.ast.Table,
+          org.asciidoctor.api.ast.Title
+      )
+    end
+
+    module Converter
+      include_package 'org.asciidoctor.converter'
+      include_package 'org.asciidoctor.api.converter'
+
+      java_import(
+          org.asciidoctor.api.converter.AbstractConverter,
+          org.asciidoctor.api.converter.Converter,
+          org.asciidoctor.api.converter.ConverterFor,
+          org.asciidoctor.api.converter.OutputFormatWriter,
+          org.asciidoctor.api.converter.StringConverter
+      )
+    end
+
     # new 'Api' namespace
     module Api
         include_package 'org.asciidoctor.api'
@@ -22,6 +73,32 @@ module AsciidoctorJ
         # Treeprocessor was renamed in to TreeProcessor in https://github.com/asciidoctor/asciidoctor/commit/f1dd816ade9db457b899581841e4cf7b788aa26d
         # This is necessary to run against both Asciidoctor 1.5.5 and 1.5.6
         TreeProcessor = Treeprocessor unless defined? TreeProcessor
+
+        java_import(
+            org.asciidoctor.api.extension.BaseProcessor,
+            org.asciidoctor.api.extension.BlockMacroProcessor,
+            org.asciidoctor.api.extension.BlockProcessor,
+            org.asciidoctor.api.extension.Contexts,
+            org.asciidoctor.api.extension.DefaultAttribute,
+            org.asciidoctor.api.extension.DefaultAttributes,
+            org.asciidoctor.api.extension.DocinfoProcessor,
+            org.asciidoctor.api.extension.ExtensionGroup,
+            org.asciidoctor.api.extension.Format,
+            org.asciidoctor.api.extension.FormatType,
+            org.asciidoctor.api.extension.IncludeProcessor,
+            org.asciidoctor.api.extension.InlineMacroProcessor,
+            org.asciidoctor.api.extension.Location,
+            org.asciidoctor.api.extension.LocationType,
+            org.asciidoctor.api.extension.MacroProcessor,
+            org.asciidoctor.api.extension.Name,
+            org.asciidoctor.api.extension.PositionalAttributes,
+            org.asciidoctor.api.extension.Postprocessor,
+            org.asciidoctor.api.extension.Preprocessor,
+            org.asciidoctor.api.extension.PreprocessorReader,
+            org.asciidoctor.api.extension.Processor,
+            org.asciidoctor.api.extension.Reader,
+            org.asciidoctor.api.extension.Treeprocessor
+        )
     end
 end
 
